7b9a247939ff4a65411b867ee1e63bd8617fae76
Nihat <senius.33@gmail.com>
2023-11-23 16:00:31 +0300

    fix module error

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { File } from 'src/entities/file.entity';
 import { User } from 'src/entities/user.entity';
-import { FileController } from './file.controller';
+import { UserController } from 'src/user/user.controller';
 import { FileService } from './file.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([File, User])],
   providers: [FileService],
-  controllers: [FileController],
+  controllers: [],
 })
 export class FileModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { File } from 'src/entities/file.entity';
 import { User } from 'src/entities/user.entity';
+import { FileService } from 'src/file/file.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([User, File])],
+  providers: [UserService, FileService],
   controllers: [UserController],
 })
 export class UserModule {}
