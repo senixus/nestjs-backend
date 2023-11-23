82baf9a732eda6466eacdd7935498471cd4d8185
Nihat <senius.33@gmail.com>
2023-11-23 15:38:15 +0300

    upload avatar method move to user.controller from file.controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,24 @@
 import { UserService } from './user.service';
-import { Body, Controller, Get, Inject, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileService } from 'src/file/file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller('user')
 export class UserController {
   @Inject() private readonly userService: UserService;
+  @Inject() private readonly fileService: FileService;
 
   @Get('/all')
   async getAll() {
@@ -19,5 +33,14 @@ export class UserController {
   @Patch(':id')
   async update(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return await this.userService.update(id, body);
+  }
+
+  @Post('avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadUserAvatar(
+    @Body() body: { userId: number },
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return await this.fileService.uploadUserAvatar(body.userId, file);
   }
 }
