b8758c15af848bfb43bc912a93df10111250783c
Nihat <senius.33@gmail.com>
2023-11-24 16:35:23 +0300

    update role guard

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -17,6 +17,6 @@ export class RolesGuard implements CanActivate {
     if (!requiredRoles) return true;
     const { user } = context.switchToHttp().getRequest();
 
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    return requiredRoles.some((role) => user.role === role);
   }
 }
